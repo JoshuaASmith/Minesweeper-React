cbcf93edfc591487f84f886cde1e43b307e5a69b
Joshua Smith <joshua.aaron.smith17@gmail.com>
2016-12-02 10:11:32 -0500

    inital minesweeper commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const React = require('react')
+
+const App = React.createClass({
+    render() {
+        return (
+            <div>
+                <h1>
+                    Hello Mines
+                </h1>
+            </div>
+        )
+    }
+})
+
+module.exports = App
